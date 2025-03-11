e6a94e279b254f104c6954ad77ff4cd9fff5b78a
Hoshino42 <122428335+Lingjunnn@users.noreply.github.com>
2025-03-11 14:38:55 +0800

    上传代码及pdf

--- experiments/throughput/throughput.py
+++ experiments/throughput/throughput.py
@@ -26,32 +26,35 @@ smoothed_throughput1 = throughput1.rolling(window=windowsize).mean()
 smoothed_throughput2 = throughput2.rolling(window=windowsize).mean()
 
 # 创建一个图形
-plt.figure(figsize=(20, 11))
+plt.figure(figsize=(17.6/2.54, 13.2/2.54))
+ax = plt.gca()
 
 # 绘制第一个数据集（Sender 1）
-plt.plot(relative_time1, smoothed_throughput1, label='Co4U', alpha=0.7, color='#d4325d')
+plt.plot(relative_time1, smoothed_throughput1, label='Co4U', color='#D4352D')
 
 # 绘制第二个数据集（Sender 2）
-plt.plot(relative_time2, smoothed_throughput2, label='Without Negotiation', alpha=0.7, color='#5ca7c7')
+plt.plot(relative_time2, smoothed_throughput2, label='Original', color='#383838')
 
 # 设置标签和标题
-plt.xlabel('Time (s)', fontsize=35)
-plt.ylabel('Throughput (Mbps)', fontsize=35)
+plt.xlabel('Time (s)', fontsize=30)
+plt.ylabel('Throughput (Mbps)', fontsize=30)
 
 # 设置坐标轴刻度
-plt.xticks(fontsize=32)
-plt.yticks(fontsize=32)
+plt.xticks(fontsize=30)
+plt.yticks(fontsize=30)
 plt.xlim(left=-100, right=4700)
 
 # 添加图例
-plt.legend(fontsize=35)
+plt.legend(loc='best', fontsize=30)
 
 # 添加网格
 plt.grid(True, linestyle='--', alpha=0.7)
 
-# 调整布局
-plt.tight_layout()
+# 直接控制坐标轴的位置和大小
+ax.set_position([0.17, 0.2, 0.78, 0.75])
+
+# 导出为PDF
+plt.savefig('throughput.pdf', bbox_inches='tight', dpi=1200)
 
 # 显示图表
-plt.savefig('throughput.pdf', format='pdf', dpi=300)
 plt.show()
